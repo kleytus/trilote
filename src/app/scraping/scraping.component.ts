630e3c25be1a145b844e7621440a33dd39a344ed
DianitaVargas <divasa52@outlook.es>
2018-06-13 00:09:43 -0400

    Scraping_Diana

--- src/app/scraping/scraping.component.ts
+++ src/app/scraping/scraping.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-scraping',
   templateUrl: './scraping.component.html',
-  styleUrls: ['./scraping.component.css']
+  styleUrls: ['./scraping.component.scss']
 })
 export class ScrapingComponent implements OnInit {
 
-  constructor() { }
+	items: Item[];
+	constructor(private _dataService: DataService) { 
+		/*this._dataService.getUsers()
+	    .map(res => this.items = res);*/
+	}
 
-  ngOnInit() {
-  }
+ 	ngOnInit() {
+  	}
+
+}
+
+interface Item {
+
+id : number;
+title : string;
+url : string; 
 
 }
