a394991ced8be088a9aa448afa649ded14853a1a
BrunoBalderrama <brunobalderrama@hotmail.com>
2018-06-28 19:25:28 -0400

    module forms

--- src/directives/error_examples.d.ts
+++ src/directives/error_examples.d.ts
@@ -5,10 +5,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-/**
- * @module
- * @description
- * Entry point for all public APIs of the common package.
- */
-import { Version } from '@angular/core';
-export declare const VERSION: Version;
+export declare const FormErrorExamples: {
+    formControlName: string;
+    formGroupName: string;
+    formArrayName: string;
+    ngModelGroup: string;
+    ngModelWithFormGroup: string;
+};
